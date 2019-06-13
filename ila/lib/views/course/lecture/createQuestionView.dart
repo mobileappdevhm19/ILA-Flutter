e6253145231f03b60c2109b3ea1ac31d0acf56b6
CevG33ko <gab.cevallos@gmail.com>
2019-06-14 00:05:20 +0200

    fixed overflowing borders added some coverage

--- ila/lib/views/course/lecture/createQuestionView.dart
+++ ila/lib/views/course/lecture/createQuestionView.dart
@@ -18,44 +18,43 @@ class _CreateQuestionViewState extends State<CreateQuestionView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Create Question')),
-      body: Column(
-        children: <Widget>[
-          ListView(
-            shrinkWrap: true,
-            children: <Widget>[
-              TextField(
-                  maxLines: 20,
-                  autofocus: true,
-                  controller: _controller,
-                  textInputAction: TextInputAction.send,
-                  decoration: InputDecoration(border: OutlineInputBorder())),
-              ButtonTheme.bar(
-                  // make buttons use the appropriate styles for cards
-                  child: ButtonBar(children: <Widget>[
-                FlatButton(
-                  child: const Text('Cancel'),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-                FlatButton(
-                  child: const Text('Post question'),
-                  //Example for Posting to API via inline generated JSON
-                  onPressed: () => widget.lecturesApi
-                          .lecturesPostQuestion(
-                              widget.lecture.id,
-                              QuestionCreate.fromJson(
-                                  {'pointedQuestion': _controller.text}))
-                          .then((_) => Navigator.of(context).pop())
-                          .catchError((error) {
-                        //TODO: handle error
-                      }),
-                ),
-              ]))
-            ],
-          )
-        ],
-      ),
+      body: SingleChildScrollView(
+
+        child: Column(
+          children: <Widget>[
+                TextField(
+                    maxLines: 20,
+                    autofocus: true,
+                    controller: _controller,
+                    textInputAction: TextInputAction.send,
+                    decoration: InputDecoration(border: OutlineInputBorder())),
+                ButtonTheme.bar(
+                    // make buttons use the appropriate styles for cards
+                    child: ButtonBar(children: <Widget>[
+                  FlatButton(
+                    child: const Text('Cancel'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  FlatButton(
+                    child: const Text('Post question'),
+                    //Example for Posting to API via inline generated JSON
+                    onPressed: () => widget.lecturesApi
+                            .lecturesPostQuestion(
+                                widget.lecture.id,
+                                QuestionCreate.fromJson(
+                                    {'pointedQuestion': _controller.text}))
+                            .then((_) => Navigator.of(context).pop())
+                            .catchError((error) {
+                          //TODO: handle error
+                        }),
+                  ),
+                ]))
+              ],
+            )
+        ),
+      
     );
   }
 }
