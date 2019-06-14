a69615af2f5d5dc14d7c1e5f34f28d6c794bac32
Philipp von Kirschbaum <info@philipp.vonkirschbaum.com>
2019-06-14 08:54:36 +0200

    Add Course Description

--- ila/lib/views/course/courseView.dart
+++ ila/lib/views/course/courseView.dart
@@ -17,19 +17,17 @@ class CourseView extends StatefulWidget {
 
 class _CourseViewState extends State<CourseView> {
   bool _isDataLectures = false;
-  bool _isDataNews = false;
+  bool _isDataCourse = false;
   List<Lecture> _lectures;
-  List<CourseNews> _news;
-
 
   @override
   void initState() {
     super.initState();
 
     widget.coursesApi.coursesGet(widget.course.id).then((course) {
-      _news = course.news;
+      this.widget.course = course;
       setState(() {
-        _isDataNews = true;
+        _isDataCourse = true;
       });
     }).catchError((error) {
       print(error.toString());
@@ -46,164 +44,170 @@ class _CourseViewState extends State<CourseView> {
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.course.title),
-      ),
-      body: Builder(
-        builder: (context) => Container(
-              padding: EdgeInsets.only(top: 15),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 25.0),
-                          child: Text(
-                            "News",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Config.PrimaryColor,
-                              fontSize: 25.0,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 15.0, right: 15.0),
-                    child: Divider(
-                      height: 10.0,
-                    ),
-                  ),
-                  _isDataNews
-                      ? ListView.separated(
-                          shrinkWrap: true,
-                          itemCount: _news.length > 2 ? 2 : _news.length,
-                          itemBuilder: (context, index) {
-                            CourseNews newsItem = _news[index];
-                            var subtitle = newsItem.body.substring(
-                                    0,
-                                    newsItem.body.length > 30
-                                        ? 30
-                                        : newsItem.body.length) +
-                                "...";
-
-                            return ListTile(
-                              title: Text(newsItem.title),
-                              subtitle: Text(subtitle),
-                              trailing: Icon(Icons.keyboard_arrow_right),
-                              onTap: () => Navigator.of(context).pushNamed(
-                                    '/newsDetails',
-                                    arguments: newsItem,
-                                  ),
-                            );
-                          },
-                          separatorBuilder: (context, index) => Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 15.0, right: 15.0),
-                              child: Divider(
-                                height: 1,
-                              )))
-                      : Center(child: CircularProgressIndicator()),
-                  Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Padding(
-                          padding:
-                              const EdgeInsets.only(left: 15.0, bottom: 10),
-                          child: GestureDetector(
-                            child: Text("Show all news",
-                                style: TextStyle(
-                                    decoration: TextDecoration.underline,
-                                    color: Colors.blue)),
-                            onTap: () => Navigator.of(context).pushNamed(
-                                  '/news',
-                                  arguments: _news,
-                                ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 25.0),
-                          child: Text(
-                            "Lectures",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Config.PrimaryColor,
-                              fontSize: 25.0,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 15.0, right: 15.0),
-                    child: Divider(
-                      height: 10.0,
-                    ),
-                  ),
-                  _isDataLectures
-                      ? ListView.separated(
-                          shrinkWrap: true,
-                          itemCount: _lectures.length,
-                          itemBuilder: (context, index) => ListTile(
-                                title: Text(_lectures[index].title),
-                                trailing: Icon(Icons.keyboard_arrow_right),
-                                onTap: () => Navigator.of(context).pushNamed(
-                                    '/lecture',
-                                    arguments: _lectures[index]),
+        appBar: AppBar(
+          title: Text(widget.course.title),
+        ),
+        body: Builder(
+          builder: (context) => Container(
+                padding: EdgeInsets.only(top: 15),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text(
+                              widget.course.description,
+                              style: TextStyle(
+                                fontSize: 15.0,
                               ),
-                          separatorBuilder: (context, index) => Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 15.0, right: 15.0),
-                              child: Divider(
-                                height: 5,
-                              )),
-                        )
-                      : Center(child: CircularProgressIndicator())
-                ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 25.0),
+                            child: Text(
+                              "News",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Config.PrimaryColor,
+                                fontSize: 25.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 15.0, right: 15.0),
+                      child: Divider(
+                        height: 10.0,
+                      ),
+                    ),
+                    _isDataCourse
+                        ? ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: widget.course.news.length > 2
+                                ? 2
+                                : widget.course.news.length,
+                            itemBuilder: (context, index) {
+                              CourseNews newsItem = widget.course.news[index];
+                              var subtitle = newsItem.body.substring(
+                                      0,
+                                      newsItem.body.length > 30
+                                          ? 30
+                                          : newsItem.body.length) +
+                                  "...";
+
+                              return Card(
+                                child: Container(
+                                  child: ListTile(
+                                    title: Text(newsItem.title),
+                                    subtitle: Text(subtitle),
+                                    trailing: Icon(Icons.keyboard_arrow_right),
+                                    onTap: () =>
+                                        Navigator.of(context).pushNamed(
+                                          '/newsDetails',
+                                          arguments: newsItem,
+                                        ),
+                                  ),
+                                ),
+                              );
+                            })
+                        : Center(child: CircularProgressIndicator()),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.only(left: 15.0, bottom: 10),
+                            child: GestureDetector(
+                              child: Text("Show all news",
+                                  style: TextStyle(
+                                      decoration: TextDecoration.underline,
+                                      color: Colors.blue)),
+                              onTap: () => Navigator.of(context).pushNamed(
+                                    '/news',
+                                    arguments: widget.course.news,
+                                  ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 25.0),
+                            child: Text(
+                              "Lectures",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Config.PrimaryColor,
+                                fontSize: 25.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 15.0, right: 15.0),
+                      child: Divider(
+                        height: 10.0,
+                      ),
+                    ),
+                    _isDataLectures
+                        ? ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: _lectures.length,
+                            itemBuilder: (context, index) => Card(
+                                    child: Container(
+                                        child: ListTile(
+                                  title: Text(_lectures[index].title),
+                                  trailing: Icon(Icons.keyboard_arrow_right),
+                                  onTap: () => Navigator.of(context).pushNamed(
+                                      '/lecture',
+                                      arguments: _lectures[index]),
+                                ))),
+                          )
+                        : Center(child: CircularProgressIndicator())
+                  ],
+                ),
               ),
-            ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.remove),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.remove),
           onPressed: () => showDialog(
               context: context,
               child: AlertDialog(
-                title: new Text("Leave Course",
-                    textAlign: TextAlign.center),
-                content: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      new FlatButton(
-                        onPressed: () => _registerButtonOnPressed(context),
-                        child: new Text("Confirm")
-                      ),
-                      new FlatButton(
-                        onPressed: (){Navigator.pop(context);},
-                        child: new Text("Cancel")
-                      ),
-                    ]
-                 )
-              )
-          ),
-      )
-    );
+                  title: new Text("Leave Course", textAlign: TextAlign.center),
+                  content: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        new FlatButton(
+                            onPressed: () => _leaveButtonOnPressed(context),
+                            child: new Text("Confirm")),
+                        new FlatButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: new Text("Cancel")),
+                      ]))),
+        ));
   }
 
-  _registerButtonOnPressed(BuildContext context) async {
+  _leaveButtonOnPressed(BuildContext context) async {
     widget.coursesApi.coursesLeave(widget.course.id).then((_) async {
       Navigator.pushNamed(context, '/home');
       scaffoldKey.currentState.showSnackBar(SnackBar(
