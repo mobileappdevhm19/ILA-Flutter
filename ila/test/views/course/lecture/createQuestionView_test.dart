6526c2cf3d8b72634c3037a6941122efb1e235d9
Philipp von Kirschbaum <info@philipp.vonkirschbaum.com>
2019-07-04 19:22:50 +0200

    CreateQuestionAnswerView Tests

--- ila/test/views/course/lecture/createQuestionView_test.dart
+++ ila/test/views/course/lecture/createQuestionView_test.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:ila/config.dart';
 import 'package:ila/models/AuthModel.dart';
 import 'package:ila/swagger/ilaApiClient.dart';
 import 'package:ila/views/course/lecture/createQuestionView.dart';
@@ -26,7 +28,75 @@ void main() {
       expect(cancelFinder, findsOneWidget);
       expect(postFinder, findsOneWidget);
     });
+
+    testWidgets('Post', (WidgetTester tester) async {
+      await tester.pumpWidget(TestHelper.buildPage(
+          CreateQuestionView(
+              _QuestionApi(),
+              Lecture.fromJson({
+                'id': 1,
+              })),
+          AuthModel(IlaApiClient()),
+          navigatorObserver: navigation));
+
+      await tester.pump();
+      await tester.enterText(find.byType(TextField).at(0), 'Frage');
+      await tester.tap(find.text('Post question'));
+
+      await tester.pumpAndSettle();
+      expect(find.text('Frage wurde gespeichert'), findsOneWidget);
+      await tester.pumpAndSettle(Duration(seconds: Config.ToastDuration));
+    });
+
+    testWidgets('Post API error', (WidgetTester tester) async {
+      await tester.pumpWidget(TestHelper.buildPage(
+          CreateQuestionView(
+              _QuestionApi(),
+              Lecture.fromJson({
+                'id': 1,
+              })),
+          AuthModel(IlaApiClient()),
+          navigatorObserver: navigation));
+
+      await tester.pump();
+      await tester.enterText(find.byType(TextField).at(0), 'falsche Frage');
+      await tester.tap(find.text('Post question'));
+
+      await tester.pumpAndSettle();
+      expect(find.text('Fehler'), findsOneWidget);
+      await tester.pumpAndSettle(Duration(seconds: Config.ToastDuration));
+    });
+
+    testWidgets('Post unknown error', (WidgetTester tester) async {
+      await tester.pumpWidget(TestHelper.buildPage(
+          CreateQuestionView(
+              _QuestionApi(),
+              Lecture.fromJson({
+                'id': 2,
+              })),
+          AuthModel(IlaApiClient()),
+          navigatorObserver: navigation));
+
+      await tester.pump();
+      await tester.enterText(find.byType(TextField).at(0), 'Frage');
+      await tester.tap(find.text('Post question'));
+
+      await tester.pumpAndSettle();
+      expect(find.text('Unbekannter Fehler ist aufgetretten'), findsOneWidget);
+      await tester.pumpAndSettle(Duration(seconds: Config.ToastDuration));
+    });
   });
 }
 
-class _QuestionApi extends QuestionApi {}
+class _QuestionApi extends QuestionApi {
+  Future<Question> questionPostQuestion(
+      int lectureId, QuestionCreate model) async {
+    if (lectureId == 1 && model.pointedQuestion == 'Frage') {
+      return null;
+    } else if (lectureId == 1) {
+      throw ApiException(400, 'Fehler');
+    } else {
+      throw 'ABC';
+    }
+  }
+}
